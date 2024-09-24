56924e2696c75a4930e10f3f39a58a607c2351a9
Numuruzero <35202735+Numuruzero@users.noreply.github.com>
2024-09-24 13:45:13 -0500

    Add connections tool

---
+++ CoolTools/ConnJS.js
@@ -0,0 +1,47 @@
+// import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
+
+setTimeout(() => {
+    const group1 = document.querySelector("#group1");
+    const group2 = document.querySelector("#group2");
+    const group3 = document.querySelector("#group3");
+    const group4 = document.querySelector("#group4");
+    const allGroups = document.querySelector("#allthings");
+
+    new Sortable(group1, {
+        group: "wordgroup",
+        swap: true, // Enable swap plugin
+        swapClass: 'highlight', // The class applied to the hovered swap item
+        animation: 150
+    });
+    new Sortable(group2, {
+        group: "wordgroup",
+        swap: true, // Enable swap plugin
+        swapClass: 'highlight', // The class applied to the hovered swap item
+        animation: 150
+    });
+    new Sortable(group3, {
+        group: "wordgroup",
+        swap: true, // Enable swap plugin
+        swapClass: 'highlight', // The class applied to the hovered swap item
+        animation: 150
+    });
+    new Sortable(group4, {
+        group: "wordgroup",
+        swap: true, // Enable swap plugin
+        swapClass: 'highlight', // The class applied to the hovered swap item
+        animation: 150
+    });
+    new Sortable(allGroups, {
+        handle: '.mover',
+        swapClass: 'highlight', // The class applied to the hovered swap item
+        animation: 150
+    });
+}, 1000)
+
+function setWords() {
+    const allWords = document.querySelector("#wordinput").value.split(",");
+    const wordPNodes = document.querySelectorAll(".word > p");
+    for (let i = 0; i < 16; i++) {
+        wordPNodes[i].innerHTML = allWords[i];
+    }
+}
